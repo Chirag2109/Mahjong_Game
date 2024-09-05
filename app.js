89e5c21b72652965723a087a0461421abb264a6e
Chirag2109 <mittalchirag010@gmail.com>
2024-09-05 16:42:45 +0530

    made the extra tile draggable and changed the theme of the game to chinese theme

--- app.js
+++ app.js
@@ -62,7 +62,6 @@ function generateTiles() {
 
     let randomTile = shuffledTemp[Math.floor(Math.random() * shuffledTemp.length)];
     let extraTileDiv = document.createElement('div');
-    extraTileDiv.classList.add('tile');
     extraTileDiv.innerText = randomTile.name;
     extraTileContainer.appendChild(extraTileDiv);
 
@@ -76,3 +75,41 @@ function generateTiles() {
 }
 
 generateTiles();
+
+dragElement(document.getElementById("extra"));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "Tile")) {
+        document.getElementById(elmnt.id + "Tile").onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
